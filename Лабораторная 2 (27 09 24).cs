2eb557492060a35532393fba392853a6c21b405e
ari-ira17 <ari.ira17@yandex.ru>
2024-10-01 20:37:58 +0600

    Add files via upload
    1) Необходимо определить количество локальных минимумов.
    2) Определить кол-во элементов, заканчивающихся на двойку.
    3) Определить 2ой максимальный элемент.
    4) Необходимо определить наибольшую длину подпоследовательности, состоящей из четных элементов.

---
+++ Лабораторная 2 (27 09 24).cs
@@ -0,0 +1,102 @@
+﻿//                          №1
+using System;
+using System.Reflection.Emit;
+class Program {
+    static void Main() 
+    {
+        int nums_of_numbers = Convert.ToInt32(Console.ReadLine());
+        int num_1 = Convert.ToInt32(Console.ReadLine());
+        int num_2 = Convert.ToInt32(Console.ReadLine());
+        int counter = 0;
+        for (int i=2; i < nums_of_numbers; i++)
+        {
+            int num_n = Convert.ToInt32(Console.ReadLine());
+            if (num_1 > num_2 && num_2 < num_n)
+            {
+                counter += 1;
+            }
+            num_1 = num_2;
+            num_2 = num_n;
+        }
+        Console.WriteLine(counter);
+  }
+}
+
+
+//                          №2
+// using System;
+// using System.Reflection.Emit;
+// class Program {
+//     static void Main() 
+//     {
+//         int n = Convert.ToInt32(Console.ReadLine());
+//         int counter = 0;
+//         for (int i=0; i<n; i++)
+//         {
+//             int a = Convert.ToInt32(Console.ReadLine());
+//             a = Math.Abs(a);
+//             if (a % 10 == 2)
+//                 counter += 1;
+//         }
+//         Console.WriteLine(counter);
+//   }
+// }
+
+
+//                          №3
+// using System;
+// using System.Reflection.Emit;
+// class Program {
+//     static void Main() 
+//     {
+//         int n = Convert.ToInt32(Console.ReadLine());
+//         int max_first = -100_000_000;
+//         int max_second = -100_000_000;
+//         for (int i=0; i<n; i++)
+//         {
+//             int a = Convert.ToInt32(Console.ReadLine());
+
+//              if (a >= max_first)
+//             {
+//                 max_second = max_first;
+//                 max_first = a;
+//             }
+//             else
+//             {
+//                 max_second = Math.Max(a, max_second);
+//             } 
+//         }
+
+//         Console.WriteLine(max_second);
+//   }
+// }
+
+
+//                          №4
+// using System;
+// using System.ComponentModel.DataAnnotations;
+// using System.Reflection.Emit;
+// class Program {
+//     static void Main() 
+//     {
+//         int n = Convert.ToInt32(Console.ReadLine());
+//         int counter_inside = 0;
+//         int counter_outside = 0;
+//         for (int i=0; i<n; i++)
+//         {
+//             int a = Convert.ToInt32(Console.ReadLine());
+//             if (a % 2 == 0)
+//             {
+//                 counter_inside += 1;
+//             }   
+//             else
+//             {
+//                 counter_inside = 0;
+//             }
+
+//             counter_outside = Math.Max(counter_inside, counter_outside);
+                
+//         }
+//         Console.WriteLine(counter_outside);
+//   }
+// }
