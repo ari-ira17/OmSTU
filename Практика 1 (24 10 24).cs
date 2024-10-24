d9892b5ef0c81e55b593459cfb61f30969cb040a
ari-ira17 <ari.ira17@yandex.ru>
2024-10-24 12:38:16 +0600

    Add files via upload
    Дан одномерный массив размерностью n. Необходимо ввод массива, преобразование массива и вывод массива оформить с помощью функций.
    Каждый элемент массива заменить на его квадрат.

---
+++ Практика 1 (24 10 24).cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Reflection.Emit;
+class Program
+{
+    static int[] Input(int num_of_numbers)
+    {
+        int[] numbers = new int[num_of_numbers];
+        for (int i = 0; i < num_of_numbers; i++)
+        {
+            Console.WriteLine("Введите число");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+            
+        return numbers;
+    }
+
+    static int[] Square(int[] numbers_square)
+    {
+        for (int i = 0; i < numbers_square.Length; i++)
+        {
+            numbers_square[i] *= numbers_square[i];
+        }
+
+        return numbers_square;
+    }
+
+    static void Output(int[] numbers_output)
+    {
+        foreach (int el in numbers_output)
+        {
+            Console.WriteLine(el);
+        }
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine("Введите длину массива");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        Output(Square(Input(n)));
+    }
+}
+
