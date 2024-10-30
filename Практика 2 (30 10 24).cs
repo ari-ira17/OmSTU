e1f9c0651ed97a2b595b8526df15b7397e2653a5
ari-ira17 <ari.ira17@yandex.ru>
2024-10-30 09:27:44 +0600

    Add files via upload
    На вход подается одномерный массив. Размерность первая подается. Необходимо:
    1) Сформировать выходной массив: сначала положительные, 0, отрицательные (сортировку использовать нельзя)
    2) Определить, является массив последовательностью равномерно убывающих элементов (5 3 2, не 5 3 1)
    3) Определить, все ли элементы четные (Да или Нет - один раз)

--- Практика 2 (30 10 24).cs
+++ Практика 2 (30 10 24).cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Net.NetworkInformation;
 using System.Reflection.Emit;
 
@@ -16,43 +17,57 @@ class Practise_2{
             numbers[i] = Convert.ToInt32(Console.ReadLine());
         }
 
+        int[] new_numbers = new int[num_of_numbers];
         // Вывод положительных
+        int k_need = 0;
         for (int k = 0; k < numbers.Length; k++)
         {
             if (numbers[k] > 0)
             {
-                Console.WriteLine(numbers[k]);
+                new_numbers[k] = numbers[k];
+                k_need = Math.Max(k, k_need);
             }
         }
+        k_need ++; //последний положительный индекс
 
         // Вывод нулевых
+        int l_need = 0;
         for (int l = 0; l < numbers.Length; l++)
         {
             if (numbers[l] == 0)
             {
-                Console.WriteLine(numbers[l]);
+                new_numbers[k_need] = numbers[l];
+                k_need ++;
+                l_need = Math.Max(l, l_need);
             }
         }
+        l_need ++;  //последний нулевой индекс
 
         // Вывод отрицательных
         for (int m = 0; m < numbers.Length; m++)
         {
             if (numbers[m] < 0)
             {
-                Console.WriteLine(numbers[m]);
+                new_numbers[l_need] = numbers[m];
+                l_need ++;
             }
         }
 
+        // 1-e задание
+        foreach (int i in new_numbers)
+            Console.Write(i + " ");
+
         // 2-ое задание
         int p = 0;
+        int t = Math.Abs(numbers[0] - numbers[1]);
         for (int j = 1; j < numbers.Length; j++)
         {
-            if (numbers[j - 1] <= numbers[j])
+            if ((numbers[j - 1] <= numbers[j]) && (numbers[j - 1] - numbers[j] == t))
             {
                 p ++;
             }
         }
-        if (p == 0) {Console.WriteLine("Последовательность убывающих элементов");}
+        if (p == 0) {Console.WriteLine("\nПоследовательность убывающих элементов");}
         else {Console.WriteLine("Не является последовательностью убывающих элементов");}
 
         // 3-е задание
