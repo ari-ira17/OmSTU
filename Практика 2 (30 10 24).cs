99f1a878c64fc264dd573323f5f9a5b5d8dfa4a7
ari-ira17 <ari.ira17@yandex.ru>
2024-10-30 08:54:11 +0600

    Add files via upload
    На вход подается одномерный массив. Размерность первая подается. Необходимо:
    1) Сформировать выходной массив: сначала положительные, 0, отрицательные (сортировку использовать нельзя)
    2) Определить, является массив последовательностью равномерно убывающих элементов (5 3 2, не 5 3 1)
    3) Определить, все ли элементы четные (Да или Нет - один раз)

---
+++ Практика 2 (30 10 24).cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Reflection.Emit;
+
+class Practise_2{
+    static void Main()
+    {
+        Console.WriteLine("Введите длину массива");
+        int num_of_numbers = Convert.ToInt32(Console.ReadLine());
+        
+        // Массив и заполнение
+        int[] numbers = new int[num_of_numbers];
+        for (int i = 0; i < num_of_numbers; i++)
+        {
+            Console.WriteLine("Введите элемент массива");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Вывод положительных
+        for (int k = 0; k < numbers.Length; k++)
+        {
+            if (numbers[k] > 0)
+            {
+                Console.WriteLine(numbers[k]);
+            }
+        }
+
+        // Вывод нулевых
+        for (int l = 0; l < numbers.Length; l++)
+        {
+            if (numbers[l] == 0)
+            {
+                Console.WriteLine(numbers[l]);
+            }
+        }
+
+        // Вывод отрицательных
+        for (int m = 0; m < numbers.Length; m++)
+        {
+            if (numbers[m] < 0)
+            {
+                Console.WriteLine(numbers[m]);
+            }
+        }
+
+        // 2-ое задание
+        int p = 0;
+        for (int j = 1; j < numbers.Length; j++)
+        {
+            if (numbers[j - 1] <= numbers[j])
+            {
+                p ++;
+            }
+        }
+        if (p == 0) {Console.WriteLine("Последовательность убывающих элементов");}
+        else {Console.WriteLine("Не является последовательностью убывающих элементов");}
+
+        // 3-е задание
+        int w = 0;
+        for (int q = 0; q < num_of_numbers; q++)
+        {
+            if (Math.Abs(numbers[q]) % 2 != 0)
+            {
+                w++;
+            }
+        }
+        if (w == 0) {Console.WriteLine("Все числа четные");}
+        else {Console.WriteLine("Не все элементы четные");}
+    
+    }
+}
