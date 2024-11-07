bf23ba3e6c7c6463ea1f39795f838832bdc8ee87
ari-ira17 <ari.ira17@yandex.ru>
2024-11-07 12:56:37 +0600

    Add files via upload
    Входной файл содержит набор строк, каждая из которых описывает отдельное действие. Строки расположены в порядке выполнения действий. 
    Выходной файл должен содержать строку, содержащую сформированное заклинание.
    Пример:
    Input.txt
    DUST root tooth
    WATER 1 tear
    Output.txt
    WTDTroottoothTDtearTW

---
+++ Зельеварение.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+class Potions_Making()
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число заклинаний");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string spell = "";
+
+            for (int i = 0; i < n; i ++)
+            {
+                string spell_now = Console.ReadLine();
+                string[] spell_now_splitted = spell_now.Split(" ");     
+                string action = spell_now_splitted[0];      
+
+                string ingredients = "";
+                for (int j = 1; j < spell_now_splitted.Length; j ++)
+                {
+                    int counter = 0;
+                    foreach (char el in spell_now_splitted[j])
+                    {
+                        if (Char.IsDigit(el))
+                        {
+                            counter++;
+                        }
+                    }
+                    
+                    if (counter == 0)
+                    {
+                        ingredients += spell_now_splitted[j];
+                    }
+
+                }       
+
+                string Spell_Now_Done(string action, string ingredients)
+                {
+                    {
+                        switch (action)
+                        {
+                            case "DUST": return "DT" + spell + ingredients + "TD";
+                            case "WATER": return "WT" + spell + ingredients + "TW";
+                            case "MIX": return "MX" + spell + ingredients + "XM";
+                            case "FIRE": return "FR" + spell + ingredients + "RF";
+                            default: return spell;
+                        }
+                    }
+                }
+
+                spell = Spell_Now_Done(action, ingredients);
+            }
+
+            Console.WriteLine(spell);
+        }
+    }
