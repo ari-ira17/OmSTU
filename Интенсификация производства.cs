1e7b54938540d5b756c67af12cb3e67ca6484250
ari-ira17 <ari.ira17@yandex.ru>
2024-11-21 17:52:00 +0600

    Add files via upload
    Перед коллективом предприятия “Ни шагу назад” была поставлена задача наращивать каждый день производство продукции на 1. 
    Требуется определить, какой суммарный объем продукции будет выпущен предприятием за заданный период, если в первый день периода предприятие выпускало P единиц продукции.

---
+++ Интенсификация производства.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Production
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите начальную дату");
+        string date_0 = Console.ReadLine();
+        string[] date_0_splitted = date_0.Split(".");
+
+        int[] date_0_int = new int[3];
+        for (int i = 0; i < 3; i ++)
+        {
+            date_0_int[i] = Convert.ToInt32(date_0_splitted[i]);
+        }
+
+        Console.WriteLine("Введите конечную дату");
+        string date_1 = Console.ReadLine();
+        string[] date_1_splitted = date_1.Split(".");
+
+        int[] date_1_int = new int[3];
+        for (int i = 0; i < 3; i ++)
+        {
+            date_1_int[i] = Convert.ToInt32(date_1_splitted[i]);
+        }
+
+        DateTime date_0_1 = new DateTime(date_0_int[2], date_0_int[1], date_0_int[0]);
+        DateTime date_1_1 = new DateTime(date_1_int[2], date_1_int[1], date_1_int[0]);
+        TimeSpan difference = date_1_1 - date_0_1;
+
+        Console.WriteLine("Введите начальный выпуск продукции");
+        int quantity = Convert.ToInt32(Console.ReadLine());
+        int quantity_now = quantity;
+        int summ = quantity;
+
+        for (int i = 0; i < difference.Days; i++)
+        {
+            quantity_now++;
+            summ += quantity_now;
+        }
+
+        Console.WriteLine($"Всего было произведено {summ}");
+    }
+}
