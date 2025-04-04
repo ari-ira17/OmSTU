f003db4d28bb1ea888b9f6d8383437bb6586964a
ari-ira17 <ari.ira17@yandex.ru>
2025-04-04 16:51:40 +0600

    Update lab8_02_04_25.py

--- Discrete_Mathematics/lab8_02_04_25.py
+++ Discrete_Mathematics/lab8_02_04_25.py
@@ -12,13 +12,13 @@ matrix = [[0, 0, 0, -1, -1],
 matrix_copy = []
 flag = True
 
-for line in matrix:     # обкладка камнями
+for line in matrix:   
     line.insert(0, -1)
     line.append(-1)
 matrix.insert(0, [-1] * len(matrix))
 matrix.append([-1] * len(matrix))
 
-if matrix[x1 - 1][y1] != -1:        # получили первые единички и с них начнем преобразовывать матрицу
+if matrix[x1 - 1][y1] != -1:       
     matrix[x1 - 1][y1] += 1
 
 if matrix[x1 + 1][y1] != -1:
@@ -32,7 +32,7 @@ if matrix[x1][y1 + 1] != -1:
 
 
 while any(0 in line for line in matrix) and flag:
-    for i in range(1, len(matrix) - 1):        # чтобы не заходить на обкладку камнями идем в тушку матрицы
+    for i in range(1, len(matrix) - 1):        
         for j in range(1, len(matrix) - 1):
 
             if matrix[i][j] != 0 and matrix[i][j] != -1:
